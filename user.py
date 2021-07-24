5533e989d3e5788d681405e8bda2e1c8358322b8
diogogosch <diogogosch@gmail.com>
2021-07-24 11:57:46 -0300

    UserClass - Class done

--- user.py
+++ user.py
@@ -17,7 +17,7 @@ class UserClass:
         if not validate_email(email):
             raise ValueError("E-mail Inválido")
         else:
-            self.__email = email
+            self.__email = email.lower()
         if len(phone_number) != 10 and len(phone_number) != 11:
             raise ValueError("Telefone Inválido")
         else:
@@ -28,3 +28,50 @@ class UserClass:
     @property
     def id(self):
         return self.__id
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name_updated):
+        self.__name = name_updated
+        self.__updated_at = datetime.now().strftime("%Y_%m_%d-%H:%M:%S")
+
+    @property
+    def cpf(self):
+        return self.__cpf
+
+    @cpf.setter
+    def cpf(self, cpf_updated):
+        if not CPF(cpf_updated):
+            raise ValueError("CPF Inválido")
+        else:
+            self.__cpf = cpf_updated
+            self.__updated_at = datetime.now().strftime("%Y_%m_%d-%H:%M:%S")
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, email_updated):
+        self.__email = email_updated
+        self.__updated_at = datetime.now().strftime("%Y_%m_%d-%H:%M:%S")
+
+    @property
+    def phone_number(self):
+        return self.__phone_number
+
+    @phone_number.setter
+    def phone_number(self, phone_number_updated):
+        self.__phone_number = phone_number_updated
+        self.__updated_at = datetime.now().strftime("%Y_%m_%d-%H:%M:%S")
+
+    @property
+    def created_at(self):
+        return self.__created_at
+
+    @property
+    def updated_at(self):
+        return self.__updated_at
