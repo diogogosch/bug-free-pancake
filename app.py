cb40a37303f29ea4ef5129c85aeaeeed31dea72a
diogogosch <diogogosch@gmail.com>
2021-07-24 17:09:01 -0300

    Tá indo, mas to fudido ao quadrado

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from user-api.user import user
+import os
+
+from flask import Flask
+
+import user
 
 user_00 = user.User('Diogo', '04188442913', 'diogogosch@gmail.com', '41988880087')
 user_01 = user.User('Ruy', '51459418972', 'rvieirag@gmail.com', '47984349315')
 
+app = Flask(__name__)
 
 if __name__ == '__main__':
     print('app.py sendo executado')
--- order-api/order.py
+++
@@ -1,7 +0,0 @@
-import user
-
-class Order:
-    order_id = itertools.count()
-
-    def __init__(self, user_id, item_description, item_quantity, item_price, total_value):
-        self.__id = next(Order.order_id)
