b1b62ee283c7adca7f5e7f816030c4bab699ee1e
diogogosch <diogogosch@gmail.com>
2021-07-24 11:04:58 -0300

    UserClass - missing timestamps

---
+++ app.py
@@ -0,0 +1,3 @@
+import user
+
+user_01 = user.UserClass('Diogo','04188442913','diogogosch@gmail.com','41988880087')
--- user.py
+++ user.py
@@ -3,18 +3,13 @@ from validate_docbr import CPF
 from validate_email import validate_email
 
 
-def valida_cpf(cpf):
-    validate_cpf = CPF()
-    return validate_cpf.validate(cpf)
-
-
-class User:
+class UserClass:
     user_id = itertools.count()
 
-    def __init__(self, name, cpf, email, phone_number, created_at, updated_at):
-        self.__id = next(User.user_id)
-        self.__name = name.lower.capitalize
-        if not valida_cpf(cpf):
+    def __init__(self, name, cpf, email, phone_number):
+        self.__id = next(UserClass.user_id)
+        self.__name = name.lower().capitalize()
+        if not CPF(cpf):
             raise ValueError("CPF Inválido")
         else:
             self.__cpf = cpf
@@ -22,12 +17,12 @@ class User:
             raise ValueError("E-mail Inválido")
         else:
             self.__email = email
-        if phone_number.len != 10 or phone_number != 11:
+        if len(phone_number) != 10 and len(phone_number) != 11:
             raise ValueError("Telefone Inválido")
         else:
             self.__phone_number = phone_number
-        self.__created_at = created_at
-        self.__updated_at = updated_at
+        # self.__created_at = created_at
+        # self.__updated_at = updated_at
 
     @property
     def id(self):
