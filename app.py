9f51aef56a535086166ee883e4d20c0e2493070b
diogogosch <diogogosch@gmail.com>
2021-07-24 12:33:56 -0300

    UserClass - refactored folders, initiated order class

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
 import user
 
-user_00 = user.UserClass('Diogo','04188442913','diogogosch@gmail.com','41988880087')
-user_01 = user.UserClass('Ruy','51459418972','rvieirag@gmail.com','47984349315')
-print(user_01.id)
-print(user_01.name)
-print(user_01.cpf)
-print(user_01.email)
-print(user_01.phone_number)
-print(user_01.created_at)
-print(user_01.updated_at)
+user_00 = user.UserClass('Diogo', '04188442913', 'diogogosch@gmail.com', '41988880087')
+user_01 = user.UserClass('Ruy', '51459418972', 'rvieirag@gmail.com', '47984349315')
 
-user_01.name = 'Henrique'
-print(user_01.name)
-print(user_01.updated_at)
 
 if __name__ == '__main__':
     print('app.py sendo executado')
---
+++ order-api/order.py
@@ -0,0 +1,3 @@
+class Order:
+
+    def __init__(self, item_description, item_quantity, item_price, total_value):
