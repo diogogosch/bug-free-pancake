d1e28a62c178a475edd401087a8bbbc028650ddc
diogogosch <diogogosch@gmail.com>
2021-07-25 16:49:29 -0300

    De grão em grão

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+SQLALCHEMY_DATABASE_URI = 'mysql://scott:tiger@localhost/my-db'
 SQLALCHEMY_TRACK_MODIFICATIONS = 'TRUE'
 
 SECRET_KEY= 'gosch'
 
 # MYSQL_HOST = "0.0.0.0"
 # MYSQL_USER = "root"
-# MYSQL_PASSWORD = "Godofwar.767"
+# MYSQL_PASSWORD =
 # MYSQL_DB = "jogoteca"
 # MYSQL_PORT = 3306
